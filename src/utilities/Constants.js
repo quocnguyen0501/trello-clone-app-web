fa6d39dad5336609ddd40d8b3ddfe187dec3ff7d
quocnguyen.forwork@gmail.com <0909343633Abc>
2022-03-31 16:37:32 +0700

    deploy trello clone app web on firebase

--- src/utilities/Constants.js
+++ src/utilities/Constants.js
@@ -1,4 +1,5 @@
-export const API_ROOT = 'http://localhost:8080';
+// export const API_ROOT = 'http://localhost:8080';
+export const API_ROOT = 'https://trello-app-apis.herokuapp.com';
 
 export const MODAL_ACTION_CLOSE = 'close';
 export const MODAL_ACTION_CONFIRM = 'confirm';
